7785d15cd5c1c663318fd7dc443e51a22ad22fa7
Atong1017 <atong1017@gmail.com>
2024-07-12 16:54:54 +0800

    Allen

--- generated_code.py
+++ generated_code.py
@@ -5,9 +5,18 @@ root = tk.Tk()
 root.geometry('500x500')
 root.title('Generated Window')
 
+entries = [
+    ('entry3', '', 64, 11, 100, 20),
+    ('entry5', '', 11, 70, 465, 250),
+]
+components = {}
+for entry_name, entry_value, x_entry, y_entry, width, height in entries:
+    components[entry_name] = tk.Entry(root)
+    components[entry_name].insert(0, entry_value)
+    components[entry_name].place(x=x_entry, y=y_entry, width=width, height=height)
+
 comboboxes = [
-    ('combobox1', ('Option 1', 'Option 2', 'Option 3', ''), 15, 19, 100, 20, ''),
-    ('combobox2', ('Option 1', 'Option 2', 'Option 3', ''), 313, 130, 100, 20, ''),
+    ('combobox4', ('Option 1', 'Option 2', 'Option 3'), 64, 41, 100, 20, 'Option 1'),
 ]
 components.update({})
 for combobox_name, values, x_combobox, y_combobox, width, height, current_value in comboboxes:
@@ -16,23 +25,20 @@ for combobox_name, values, x_combobox, y_combobox, width, height, current_value 
     components[combobox_name].set(current_value)
 
 labels = [
-    ('label3', '自訂組數:', 50, 110, 100, 20),
-    ('label4', '連錯換單:', 50, 140, 100, 20),
-    ('label5', '%', 50, 170, 100, 20),
-    ('label6', '%', 50, 200, 100, 20),
-    ('label7', '歷史筆數', 50, 230, 100, 20),
-    ('label8', '下單組數(-+):', 50, 260, 100, 20),
-    ('label9', '次後不再下注:', 50, 290, 100, 20),
-    ('label10', '二次換單:', 48, 322, 100, 20),
-    ('label11', '起始金額:', 130, 10, 100, 20),
-    ('label12', '每單籌碼:', 130, 40, 100, 20),
-    ('label13', '模擬器:', 130, 70, 100, 20),
-    ('label14', '更新排名:', 130, 100, 100, 20),
-    ('label15', '內無則扣除:', 130, 130, 100, 20),
+    ('label1', '筆數:', 10, 10, 34, 18),
+    ('label2', '種類:', 10, 40, 34, 17),
 ]
 components.update({})
 for label_name, text, x_label, y_label, width, height in labels:
     components[label_name] = tk.Label(root, text=text)
     components[label_name].place(x=x_label, y=y_label, width=width, height=height)
+
+buttons = [
+    ('button6', '計算', 348, 336, 100, 20),
+]
+components.update({})
+for button_name, text, x_button, y_button, width, height in buttons:
+    components[button_name] = tk.Button(root, text=text)
+    components[button_name].place(x=x_button, y=y_button, width=width, height=height)
 
 root.mainloop()
